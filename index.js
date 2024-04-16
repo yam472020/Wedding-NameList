5d961e9cb655cc0ce20305c5e1b3b62af206644d
yam472020 <53906582+yam472020@users.noreply.github.com>
2024-04-16 15:14:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function displayResults(results) {
     //自動選取唯一筆的名子
     if (results.length == 1) {
         //取消聚焦
-        document.body.focus()
+        document.activeElement.blur();
         autoclick()
     } else {
         // 初始化樣式
